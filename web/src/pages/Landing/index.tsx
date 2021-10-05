52d5f544f3188e4ca40798210811c39d81aa7986
Iann-rst <iannrst11@gmail.com>
2021-10-05 16:04:37 -0300

    Página Home (Landing page)

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 /*Arquivo de estilização da landing page */
 import './styles.css';
@@ -26,15 +27,15 @@ function Landing(){
         />
 
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar" />
             Estudar
-          </a>
+          </Link>
 
-          <a href="" className="give-classes">
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="Dar Aulas" />
             Dar Aulas
-          </a>
+          </Link>
         </div>
 
         <span className="total-connections">
