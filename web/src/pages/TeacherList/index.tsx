b2982d7c3b2359441eaa7a98ab2d77d3587ac6e3
Iann-rst <iannrst11@gmail.com>
2021-10-05 20:48:35 -0300

    Add: formulário dos proffys

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 /* Arquivo de estilização */
 import './styles.css';
 
@@ -22,9 +23,36 @@ function TeacherList(){
             <label htmlFor="time">Hora</label>
             <input type="text" id="time"/>
           </div>
-
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img src="https://avatars.githubusercontent.com/u/48128325?v=4" alt="Iann Rodrigues" />
+            <div>
+              <strong>Iann Rodrigues</strong>
+              <span>Química</span>
+            </div>
+          </header>
+          <p>
+            Entusiasta das melhores tecnologias de química avançada.
+            <br/><br/>
+            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.
+          </p>
+
+          <footer>
+            <p>
+              Preço/hora
+              <strong>R$80,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="whatsapp" />
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   )
 }
