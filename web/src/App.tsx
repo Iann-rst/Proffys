7cf89ef7cbbf1119d85f7aa541ebe03b03347d73
Iann-rst <iannrst11@gmail.com>
2021-10-05 12:21:49 -0300

    Add: Landing Page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 /* Arquivo de estilização */
 import './assets/styles/global.css';
+import Landing from './pages/Landing';
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <Landing/>
     );
 }
 
