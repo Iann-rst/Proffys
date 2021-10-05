008ef15aaf59ee8edd6dba5f1694bb436e14559f
Iann-rst <iannrst11@gmail.com>
2021-10-05 20:57:39 -0300

    Add: componente TeacherItem para listar os proffys

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 /* Arquivo de estilização */
 import './styles.css';
 
@@ -27,31 +27,10 @@ function TeacherList(){
       </PageHeader>
 
       <main>
-        <article className="teacher-item">
-          <header>
-            <img src="https://avatars.githubusercontent.com/u/48128325?v=4" alt="Iann Rodrigues" />
-            <div>
-              <strong>Iann Rodrigues</strong>
-              <span>Química</span>
-            </div>
-          </header>
-          <p>
-            Entusiasta das melhores tecnologias de química avançada.
-            <br/><br/>
-            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.
-          </p>
-
-          <footer>
-            <p>
-              Preço/hora
-              <strong>R$80,00</strong>
-            </p>
-            <button type="button">
-              <img src={whatsappIcon} alt="whatsapp" />
-              Entrar em contato
-            </button>
-          </footer>
-        </article>
+        <TeacherItem/>
+        <TeacherItem/>
+        <TeacherItem/>
+        <TeacherItem/>
       </main>
     </div>
   )
