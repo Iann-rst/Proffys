9fe6ff4ea5d1fed6ac97c12ee773214431628478
Iann-rst <iannrst11@gmail.com>
2021-10-06 11:30:14 -0300

    Servidor

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
-app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello World'});
-})
+app.use(express.json());
+app.use(routes);
+
 
 //localhost:3333
 app.listen(3333);
