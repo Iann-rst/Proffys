92c1363dbc55f1595ac7c92716f0853abd1da081
Iann-rst <iannrst11@gmail.com>
2021-10-06 17:13:45 -0300

    PageHeader (Utilizando Props)

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -7,8 +7,11 @@ import backIcon from '../../assets/images/icons/back.svg';
 /* Arquivo de estilização*/
 import './styles.css';
 
+/* Propriedades do componente Header das páginas:
+*** Study (TeacherList) e Give-Classes (TeacherForm) */
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) =>{
@@ -22,10 +25,15 @@ const PageHeader: React.FC<PageHeaderProps> = (props) =>{
     </div>
 
     <div className="header-content">
+      
+      {/*props para o titulo no Header da página */}
       <strong>{props.title}</strong>
+      
+      {/*props para a descrição no Header da página */}
+      {props.description && <p>{props.description}</p>} 
 
       {/*Recebendo o formulário da TeacherList por props */}
-    {props.children}
+      {props.children}
     </div>
   </header>
   );
