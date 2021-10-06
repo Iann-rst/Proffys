c95233fa4aee3d71d3cf2d925765a1816afca6d4
Iann-rst <iannrst11@gmail.com>
2021-10-06 11:20:54 -0300

    Criação da tabela Class_schedule

---
+++ server/src/database/migrations/02_create_class_schedule.ts
@@ -0,0 +1,23 @@
+import {Knex} from 'knex';
+
+export async function up(knex: Knex){
+  return knex.schema.createTable('class_schedule', table =>{
+    table.increments('id').primary();
+
+    table.integer('week_day').notNullable();
+    table.integer('from').notNullable();
+    table.integer('to').notNullable();
+
+    /* Chave estrangeira (foreign_key) referenciando a chave primaria (primary key) da tabela user */
+    table.integer('class_id')
+    .notNullable()
+    .references('id')
+    .inTable('users')
+    .onUpdate('CASCADE')
+    .onDelete('CASCADE');
+  });
+}
+
+export async function down(knex: Knex){
+  return knex.schema.dropTable('class_schedule');
+}
