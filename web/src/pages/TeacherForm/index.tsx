fa0a0f74018db7f5a9f2f441423ae7b473667b03
Iann-rst <iannrst11@gmail.com>
2021-10-06 19:15:00 -0300

    Add: Horários disponíveis no formulário

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -52,16 +52,48 @@ function TeacherForm(){
               {value: 'Química', label: 'Química'},
             ]}
           /> 
+
+          {/* Componente para os inputs */}
           <Input name="cost" label="Custo da sua hora por aula"/>
-       </fieldset>
-       <footer>
-         <p>
-           <img src={warningIcon} alt="Aviso importante" />
-           Importante! <br/>
-           Preencha todos os dados
-         </p>
-         <button type="button">Salvar cadastro</button>
-       </footer>
+       </fieldset> 
+       
+      <fieldset>
+        <legend>
+          Horários Disponíveis
+          <button type="button">
+          + Novo horário
+        </button>
+        </legend>
+
+        <div className="schedule-item">
+          {/* Componente para o select */}
+          <Select 
+            name="week_day" 
+            label="Dia da semana"
+            options={[
+                {value: '0', label: 'Domingo'},
+                {value: '1', label: 'Segunda-feira'},
+                {value: '2', label: 'Terça-feira'},
+                {value: '3 ', label: 'Quarta-feira'},
+                {value: '4', label: 'Quinta-feira'},
+                {value: '5', label: 'Sexta-feira'},
+                {value: '6', label: 'Sábado'},
+              ]}
+          />
+          {/* Componente para os inputs */}
+          <Input name="from" label="Das" type="time"/>
+          <Input name="to" label="Até" type="time"/>
+        </div>
+      </fieldset>    
+
+      <footer>
+        <p>
+          <img src={warningIcon} alt="Aviso importante" />
+          Importante! <br/>
+          Preencha todos os dados
+        </p>
+        <button type="button">Salvar cadastro</button>
+      </footer>
 
       </main>
 
