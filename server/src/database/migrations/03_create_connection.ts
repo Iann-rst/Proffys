db47fb5d736be7d1ff136245d84e899496f1c02f
Iann-rst <iannrst11@gmail.com>
2021-10-06 11:21:54 -0300

    Criação da tabela Connection(Conexão aluno-proffy)

---
+++ server/src/database/migrations/03_create_connection.ts
@@ -0,0 +1,25 @@
+import {Knex} from 'knex';
+
+/* Conexão entre professor e aluno */
+
+export async function up(knex: Knex){
+  return knex.schema.createTable('connections', table =>{
+    table.increments('id').primary();
+
+    /* Chave estrangeira (foreign_key) referenciando a chave primaria (primary key) da tabela user */
+    table.integer('user_id')
+    .notNullable()
+    .references('id')
+    .inTable('users')
+    .onUpdate('CASCADE')
+    .onDelete('CASCADE');
+
+    table.timestamp('create_at')
+    .defaultTo('now()')
+    .notNullable();
+  });
+}
+
+export async function down(knex: Knex){
+  return knex.schema.dropTable('connections');
+}
