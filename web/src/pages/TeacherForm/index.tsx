ea0e62df8378b4bd6da9092fa02a76b19ab8ec7b
Iann-rst <iannrst11@gmail.com>
2021-10-06 17:05:50 -0300

    Página cadastro de proffys (TeacherForm)

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
 
 /* Arquivo de estilização */
 import './styles.css';
@@ -7,7 +10,38 @@ import './styles.css';
 function TeacherForm(){
   return(
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas."/>
+      <PageHeader 
+        title="Que incrível que você quer dar aulas."
+        description="O primeiro passo é preencher esse formulário de inscrição"
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <Input name="name" label="Nome completo"/>
+          <Input name="avatar" label="Avatar"/>
+          <Input name="whatsapp" label="WhatsApp"/>
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+
+          <Input name="subject" label="Matéria"/>
+          <Input name="cost" label="Custo da sua hora por aula"/>
+       </fieldset>
+       <footer>
+         <p>
+           <img src={warningIcon} alt="Aviso importante" />
+           Importante! <br/>
+           Preencha todos os dados
+         </p>
+         <button type="button">Salvar cadastro</button>
+       </footer>
+
+      </main>
+
+      
     </div>
   )
 }
