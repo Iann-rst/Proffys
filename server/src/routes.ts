50622d1baae4ce24fc0205d7e12d4d3b56472e52
Iann-rst <iannrst11@gmail.com>
2021-10-06 12:09:43 -0300

    Criação da rota para criar uma aula

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,70 @@
 import express from 'express';
+import db from './database/connection';
+import convertHourToMinutes from './utils/convertHourToMinutes';
 
 const routes = express.Router();
 
-routes.post('/classes', (request, response) => {
-  const data = request.body;
+interface ScheduleItem{
+  week_day: number;
+  from: string;
+  to: string;
+}
 
-  console.log(data);
-  return response.send();
+/* Rota para criar uma aula */
+routes.post('/classes', async (request, response) => {
+  const {
+    name,
+    avatar,
+    whatsapp,
+    bio,
+    subject,
+    cost,
+    schedule
+  } = request.body;
+
+  const trx = await db.transaction();
+
+  try{
+    const insertedUsersIds = await trx('users').insert({
+      name, 
+      avatar, 
+      whatsapp, 
+      bio,
+    });
+  
+    //pega o id do primeiro usuário que foi inserido no banco
+    const user_id = insertedUsersIds[0];
+  
+    const insertedClassesIds = await trx('classes').insert({
+      subject, 
+      cost,
+      user_id,
+    });
+  
+    //Pega o id da primeira classe (materia) criada no banco
+    const class_id = insertedClassesIds[0];
+    
+    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+      return{
+        class_id,
+        week_day: scheduleItem.week_day,
+        from: convertHourToMinutes(scheduleItem.from),
+        to: convertHourToMinutes(scheduleItem.to),
+      };
+    })
+  
+    await trx('class_schedule').insert(classSchedule);
+  
+    await trx.commit();
+
+    return response.status(201).send();
+
+  }catch (err){
+    await trx.rollback();
+    return response.status(400).json({
+      error: 'Unexpected error while creating new class'
+    })
+  }
 });
 
 export default routes;
