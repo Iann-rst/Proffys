9fb3003c2082660e6c9f5329b64faeec30d9de17
Iann-rst <iannrst11@gmail.com>
2021-10-07 15:19:05 -0300

    Consumir API para mostrar o professor disponível

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -5,31 +5,43 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 /*Arquivo de Estilização */
 import './styles.css';
 
-function TeacherItem(){
+export interface Teacher{
+  
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+  
+}
+
+interface TeacherItemProps{
+  teacher: Teacher
+}
+
+function TeacherItem({teacher}: TeacherItemProps){
   return(
     <article className="teacher-item">
       <header>
-        <img src="https://avatars.githubusercontent.com/u/48128325?v=4" alt="Iann Rodrigues" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Iann Rodrigues</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-        <p>
-          Entusiasta das melhores tecnologias de química avançada.
-          <br/><br/>
-          Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.
-        </p>
+        <p>{teacher.bio}</p>
 
         <footer>
           <p>
             Preço/hora
-            <strong>R$80,00</strong>
+            <strong>R$ {teacher.cost}</strong>
           </p>
-          <button type="button">
+          <a href={`https://wa.me/${teacher.whatsapp}`}>
             <img src={whatsappIcon} alt="whatsapp" />
-            Entrar em contato
-          </button>
+            Entrar em contato.
+          </a>
         </footer>
     </article>
   );
