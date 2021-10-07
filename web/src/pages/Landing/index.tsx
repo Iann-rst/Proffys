a6fb237fb0994151f25e2a201b3675064ce60ca4
Iann-rst <iannrst11@gmail.com>
2021-10-07 11:55:03 -0300

    Receber 'totalConnection' da API (Banco de dados)

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {Link} from 'react-router-dom'
+
+/* Conectar o front-end com o back-end. Consumindo api do banco de dados (back-end) */
+import api from '../../services/api';
 
 /*Arquivo de estilização da landing page */
 import './styles.css';
@@ -12,6 +15,17 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 function Landing(){
+
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(()=>{
+    api.get('connections').then(response =>{
+      const {total} = response.data;
+
+      setTotalConnections(total);
+    })
+  },[]);
+
   return(
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -39,7 +53,7 @@ function Landing(){
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração Roxo"/>
+          Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração Roxo"/>
         </span>
       </div>
     </div>
