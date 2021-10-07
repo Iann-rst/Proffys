13aa633f0356d0d7e93687a2173a875fe595e8c1
Iann-rst <iannrst11@gmail.com>
2021-10-07 11:41:00 -0300

    Servidor

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,12 +3,14 @@ import cors from 'cors';
 import routes from './routes';
 
 
+
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
 
 //localhost:3333
+console.log('Conectado...');
 app.listen(3333);
