2a43624b28d741ea4435a65755e93e20f9b73d77
Iann-rst <iannrst11@gmail.com>
2021-10-07 15:20:13 -0300

    Buscar na API (banco de dados) os proffys.

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
 import Select from '../../components/Select';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher} from '../../components/TeacherItem';
+import api from '../../services/api';
 
 /* Arquivo de estilização */
 import './styles.css';
 
+
 function TeacherList(){
+
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
+
+  const[teachers, setTeachers] = useState([]);
+
+  /* Consultar a api (banco de dados) para buscar 
+  * professor de acordo com a materia, dia e horário
+  */
+  async function searchTeachers(e: FormEvent){
+    e.preventDefault();
+    
+    const response = await api.get('classes', {params: {
+      subject,
+      week_day,
+      time
+    }});
+
+    setTeachers(response.data);
+  }
+
   return(
     <div id="page-teacher-list" className="container">
       {/* Componente PageHeader (Header da página) 
@@ -15,12 +39,16 @@ function TeacherList(){
       */}
       <PageHeader title="Estes são os proffys disponíveis.">
 
-        {/*Passando o formulário como props.children para o componente PageHeader*/}
-        <form id="search-teachers">
+        {/*Passando o formulário como props.children(
+          <PageHeader>FORMULÁRIO</PageHeader>
+          ) para o componente PageHeader*/}
+        <form id="search-teachers" onSubmit={searchTeachers}>
           
           <Select 
             name="subject" 
             label="Matéria"
+            value={subject}
+            onChange={(e)=>{setSubject(e.target.value)}}
             options={[
               {value: 'Artes', label: 'Artes'},
               {value: 'Biologia', label: 'Biologia'},
@@ -37,6 +65,8 @@ function TeacherList(){
           <Select 
             name="week_day" 
             label="Dia da semana"
+            value={week_day}
+            onChange={(e)=>{setWeekDay(e.target.value)}}
             options={[
               {value: '0', label: 'Domingo'},
               {value: '1', label: 'Segunda-feira'},
@@ -49,16 +79,23 @@ function TeacherList(){
           />
 
           {/* Utilizando componente Input, para representar os inputs do formulário*/}
-          <Input type="time" name="time" label="Hora"/>
+          <Input 
+            type="time" 
+            name="time" 
+            label="Hora"
+            value={time}
+            onChange={(e)=>{setTime(e.target.value)}}
+          />
+          {/* Adicionando botão para o submit do formulário */}
+          <button type="submit">Buscar</button>
         </form>
       </PageHeader>
       
       {/* Listagem dos proffys */}
       <main>
-        <TeacherItem/>
-        <TeacherItem/>
-        <TeacherItem/>
-        <TeacherItem/>
+        {teachers.map((teacher: Teacher) => {
+          return <TeacherItem key={teacher.id} teacher={teacher}/>;
+        })}
       </main>
     </div>
   )
